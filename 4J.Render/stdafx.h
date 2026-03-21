da48e8713ebc161f242467ed94e41566134a812e
JuiceyDev <me@juicey.dev>
2026-03-21 17:05:54 +0100

    changes and shit

--- 4J.Render/stdafx.h
+++ 4J.Render/stdafx.h
@@ -1,6 +1,9 @@
 #ifndef _4J_RENDER_STADAFX_H
 #define _4J_RENDER_STADAFX_H
 
+#ifdef __linux__
+#include "../../../Minecraft.Client/Platform/Linux/Stubs/winapi_stubs.h"
+#endif
 #include "../4J.Common/4J_Compat.h"
 
 #endif  //_4J_RENDER_STADAFX_H
